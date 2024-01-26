1f6a909c5ce15fc098478ec552f8a8c5d623b033
FirePlank <44502537+FirePlank@users.noreply.github.com>
2024-01-26 14:22:35 +0200

    small improvements

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import asyncio
 import os
-import re
 import subprocess
 import discord
+import platform
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
 
 intents = discord.Intents.default()
 intents.message_content = True
+allowed_mentions = discord.AllowedMentions(roles=False, users=False, everyone=False)
+bot = commands.Bot(command_prefix='-', intents=intents, allowed_mentions=allowed_mentions)
 
-bot = commands.Bot(command_prefix='-', intents=intents)
-
-discord.AllowedMentions(everyone=False, roles=False, users=False)
 
 @bot.event
 async def on_ready():
@@ -20,20 +22,19 @@ async def on_ready():
 
 @bot.command()
 async def run(ctx, *, code: str):
-    """Runs a block of shortlang code"""
-
-    if ctx.author.id != 873410122616037456:
-        return
+    """Runs a block of ShortLang code"""
 
     code = code.replace('```', '')
-    print(f"code is: {code}")
-
-    with open("tmp.sl", "w") as f:
+    with open("program.sl", "w") as f:
         f.writelines(code)
+
+    command = "shortlang"
+    if platform.system() == "Windows":
+        command += ".exe"
 
     try:
         process = await asyncio.create_subprocess_shell(
-            "shortlang tmp.sl",
+            f"{command} program.sl",
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             limit=4096  # Set a reasonable limit for the output
@@ -47,8 +48,7 @@ async def run(ctx, *, code: str):
 
         # response = f"**Output:**\n```\n{stdout.decode()[:1500]}\n```\n**Error:**\n```\n{stderr.decode()}\n```"
 
-        embed = discord.Embed()
-
+        embed = discord.Embed(color=discord.colour.parse_hex_number("8080FF"))
         stdout = stdout.decode()
 
         if len(stdout) != 0:
@@ -57,14 +57,11 @@ async def run(ctx, *, code: str):
             output = "*No output.*"
 
         embed.add_field(name="Program Output", value=output, inline=False)
-
         stderr = stderr.decode()
-        embed.color = discord.colour.parse_hex_number("8080FF")
 
         if len(stderr) != 0:
             embed.add_field(name="Compiler output", value=f"```\n{stderr[:500]}\n```", inline=False)
             embed.color = discord.colour.parse_hex_number("FF0000")
-
 
         await ctx.reply(embed=embed)
 
@@ -72,5 +69,4 @@ async def run(ctx, *, code: str):
         await ctx.reply("The process timed out.")
 
 
-
-bot.run(os.getenv("TOKEN"))
+bot.run(os.getenv("DISCORD_TOKEN"))
